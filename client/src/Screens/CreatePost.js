47506b0e45115cdf1be50207101c5c5713beda54
SARB <mrsinghbusiness05@gmail.com>
2023-02-20 07:50:05 +0530

    Feature - Image View after Selection

--- client/src/Screens/CreatePost.js
+++ client/src/Screens/CreatePost.js
@@ -1,55 +1,68 @@
 import axios from 'axios';
 import React ,{ useState}  from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+// import { useNavigate } from 'react-router-dom';
+// import { toast } from 'react-toastify';
 
 
 const CreatePost = () => {
 
-    const [title,settitle] = useState("");
-    const [body,setbody] = useState("");
+    // const [title,settitle] = useState("");
+    // const [body,setbody] = useState("");
+
     const [image,setimage] = useState("");      // for getting Image 
-    const [url,seturl] = useState("");          // for Img URL 
     
-    const navigate = useNavigate();
+    // const [url,seturl] = useState("");
+    
+    
+    const [fileinputState,setfileinputState] = useState("");
+    const [selectedFile,setselectedFile] = useState("");
 
-        // const handlepostsubmit = () => {
+    const [PreviewSource, setPreviewSource] = useState('');
 
-        // const data = new FormData()
-        // data.append("file",image)
-        // data.append('upload_preset',"insta-clone")
-        // data.append('cloud_name',"damnzg3hr")
-        // fetch('https://api.cloudinary.com/v1_1/damnzg3hr/image/upload' , {
-        //     method : 'post',
-        //     body : data
-        // })
-        // .then(res => res.json())
-        // .then(data =>  {
-        //     // console.log(data)
-        //     seturl(data.url)
-        // })
-        // .catch(err =>  {console.log(err)})
+    // const handleimagepost = () => {
+    //     const data = new FormData()
+    //     data.append("file",image)
+    //     data.append("upload_preset","insta-clone")
+    //     data.append("cloud_name","damnzg3hr")
 
-        //  fetch('/createpost',{
-        //     method : "post",
-        //     headers : {
-        //         "Content-Type" : 'application/json'
-        //     },
-        //     body : JSON.stringify({
-        //       title,
-        //       body,
-        //       photo: url
-        //     })
-        //  }).then(res => res.json())
-        //  .then(data => { console.log(data)})
+    //     // fetch('https://api.cloudinary.com/v1_1/damnzg3hr/image/upload' ,{
+    //     //     method : "post",
+    //     //     body : data
+    //     // }).then((res) => res.json())
+    //     // .then((data) => {
+    //     //      console.log(data);
+    //     // })
+    //     // .catch((err) => {
+    //     //     console.log(err);
+    //     // })
 
-        //  if(data.err){
-        //         toast.error(' Error here in data ')
-        //  }else{
-        //     toast.success(' Create post Data Posted ')
-        //  }
-        // }
+    //     const previewfile = (file) => {
+    //         const reader = new FileReader();
+    //         reader.readAsDataURL(file);
+    //         reader.onloadend  = () => {
+    //             setPreviewSource(reader.result);
+    //         }
+    // }
 
+    //     axios.post('https://api.cloudinary.com/v1_1/damnzg3hr/image/upload' ,data)
+    //     .then((res) => {
+    //         console.log('res image is -',res.data);
+    //     }).catch((err) => console.log('error is -',err));        
+    // }
+
+    const  handlefileinputChange = (e) => {
+
+        const file = e.target.files[0];
+        previewFile(file)
+    }
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        }
+    }
 
   return (
     <div>
@@ -58,11 +71,16 @@ const CreatePost = () => {
                         <input type = "text" placeholder = 'Enter Title...' />
                         <input type = "text" placeholder = 'Enter Body...' />
                   
-                             <label for="file-upload" id ="file-upload">
-                                Custom Upload
+                             <label htmlFor = "file-upload" id = "file-upload">
+                             
                             </label>
-                            <input id="file-upload" type="file" id = ""/>
-                        <button> Submit Post  </button> 
+                            <input  type = "file"  onChange = {handlefileinputChange} 
+                             value = {fileinputState}  id = "file-upload" />
+                             {PreviewSource && (
+                                <img src = {PreviewSource}  alt = "chosen"  style = {{width:'100%'}} />   ) 
+                             }
+                            {/* <button onClick = {handleimagepost}> Submit Post  </button> */}
+                           {/* URL here is -  {url}  */}
                     </div>
             </div>
     </div>
